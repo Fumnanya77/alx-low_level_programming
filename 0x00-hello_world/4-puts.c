1035cc3a376d61b12849fffd4d992efed474c3bb
Fumnanya77 <odohifunanyaj@gmail.com>
2022-09-08 19:20:48 -0700

    Output a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: This will print out a string
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}	
