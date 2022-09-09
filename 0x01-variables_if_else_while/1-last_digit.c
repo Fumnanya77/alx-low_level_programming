93bc9b7775627c23ab5729ce355a2dddd70c8c44
Fumnanya77 <odohifunanyaj@gmail.com>
2022-09-09 16:20:58 -0700

    The fate of the last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Print: The last digit from a random input
+ *
+ * Check: Assign positive or negative to the number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int l_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l_digit = n % 10;
+	if (l_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
+	} else if (l_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l_digit);
+	} else if ((l_digit < 6) && (l_digit != 0))
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_digit);
+	} else
+	{
+		printf("This is not a number");
+	}
+	return (0);
+}
