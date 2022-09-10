5e24e129d4e3f7f4383d0ad5392f024d92ca2671
Fumnanya77 <odohifunanyaj@gmail.com>
2022-09-09 19:19:31 -0700

    All possible combinations of a single number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Print: All possible combinations of a single digit
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
