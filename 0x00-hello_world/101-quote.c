2a1193a6e63e0cacc9b4b9d2bdb4db2f2cb8a325
Fumnanya77 <odohifunanyaj@gmail.com>
2022-09-10 14:46:09 -0700

    Quote by Dora

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Print: Print out without puts or printf
+ *
+ * Return: For this code 1 (Success)
+ */
+
+int main(void)
+{
+	char *quote = "and that piece of art is usefule\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stdout, "%s", quote);
+	return (1);
+}
