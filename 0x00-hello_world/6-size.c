02575bd3d558d72a3fefe9e645c80a720863ae8a
Fumnanya77 <odohifunanyaj@gmail.com>
2022-09-10 14:00:20 -0700

    size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-# !/ bin / bash
 #include <stdio.h>
 
 /**
@@ -11,10 +10,10 @@
 
 int main(void)
 {
-	puts("Size of char: %lu byte(s)", sizeof(char));
-	puts("Size of int: %lu byte(s)", sizeof(int));
-	puts("Size of long int: %lu byte(s)", sizeof(long int));
-	puts("Size of long long int: %lu bytes(s)", sizeof(long long int));
-	puts("Size of float: %lu byte(s)", sizeof(float));
+	printf("Size of char: %u byte(s)\n", sizeof(char));
+	printf("Size of int: %u byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
