977d0e3b1051c35eb515603f00e2dbc6e509ab65
Fumnanya77 <odohifunanyaj@gmail.com>
2022-09-13 11:02:34 -0700

    Using putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Print: output putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *ch = "_putchar";
+
+	while (*ch != '\0')
+	{
+		putchar(*ch);
+		ch = ch + 1;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains scripts or programs demostrating functions and nested loops
