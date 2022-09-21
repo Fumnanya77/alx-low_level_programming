5083533b7ed5ffc33dbaaff7b7cd749e11fecbdc
Fumnanya77 <odohifunanyaj@gmail.com>
2022-09-21 06:37:22 -0700

    print a string to the stdout from a pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - This pronts the value of the pointer's Variable
+ *
+ * @str: the pointer variable to the string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
