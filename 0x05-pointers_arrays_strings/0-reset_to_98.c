d67d101bf51979c25684cc147c5568a5f3f09e94
Fumnanya77 <odohifunanyaj@gmail.com>
2022-09-21 04:45:48 -0700

    change the pointer variable value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of the variable of pointer to 98
+ *
+ * @n: the pointer variable
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory is on pointers and arrays and strings
