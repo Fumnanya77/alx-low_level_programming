77b7785879e4beb1fd20796b746a93e236eeae0e
Fumnanya77 <odohifunanyaj@gmail.com>
2022-09-29 14:06:32 -0700

    recursive reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,11 +10,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i, lenstr;
-
-	lenstr = strlen(s);
-	for (i = lenstr; i >= 0; i--)
+	if (*s)
 	{
-		_putchar(s[i]);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
