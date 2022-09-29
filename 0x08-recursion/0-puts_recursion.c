e7fa1dc0047afaee13862d002e8f17d0dec4066f
Fumnanya77 <odohifunanyaj@gmail.com>
2022-09-29 10:59:54 -0700

    puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string usinng outs fn
+ *
+ * @s: the string variaable
+ *
+ * Return:Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	char str[20];
+
+	strcpy(str, s);
+
+	puts(str);
+}
