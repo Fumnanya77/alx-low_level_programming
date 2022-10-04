78b2bd043866e528aa55e60ca45f9da39065ad78
Fumnanya77 <odohifunanyaj@gmail.com>
2022-10-04 07:00:56 -0700

    duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strdup - print a string from a duplicate memory
+ *
+ * @str: pointer to the duplicate string of str
+ *
+ * Return: Pointer to the duplicate string
+ */
+
+char *_strdup(char *str)
+{
+	char *maldup;
+
+	maldup = malloc(sizeof(*str));
+	maldup = strdup(str);
+
+	return (maldup);
+}
