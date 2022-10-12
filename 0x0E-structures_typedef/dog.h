be9d5fc1682f18bc37a2438ce4be477c5bf39686
Fumnanya77 <odohifunanyaj@gmail.com>
2022-10-11 19:45:39 -0700

    structures in C language

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,14 @@
+#ifndef MY_DOG
+#define MY_DOG
+
+/**
+ * struct dog - Properties of a dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
@@ -7,3 +18,5 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+#endif
