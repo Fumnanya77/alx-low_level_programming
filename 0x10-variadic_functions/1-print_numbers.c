3468bb33b0e5b586f4098bc3f4490adc0028b101
Fumnanya77 <odohifunanyaj@gmail.com>
2022-10-13 18:31:10 -0700

    variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ap, int));
 		if (i < (n - 1))
-			printf("%c ", *separator);
+			vprintf(separator, ap);
 	}
 	printf("\n");
 }
