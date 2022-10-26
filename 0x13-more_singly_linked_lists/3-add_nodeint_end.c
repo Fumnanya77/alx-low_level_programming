e1a04644025eb01cd8bf2e04b30f85e2a5cdbaca
Fumnanya77 <gilbertgabriellafrances@gmail.com>
2022-10-25 22:01:11 -0700

    linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 
 /**
  * add_nodeint_end - add a linked list node at the end
@@ -9,7 +9,7 @@
  * Return: an address to the added node
  */
 
-listint_t add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *last;
@@ -26,5 +26,6 @@ listint_t add_nodeint_end(listint_t **head, const int n)
 	while (last->next != NULL)
 		last = last->next;
 	last->next = new;
+	return (last->next);
 	return (new);
 }
