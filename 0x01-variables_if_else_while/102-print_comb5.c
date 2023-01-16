b6c9bf39912d4249a50cc819ee929e7f2edf7acf
= <=>
2023-01-16 00:24:16 -0800

    combination combo :)

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point; print all possible combination of two two-digits numbers
+ *
+ * Return: Always 0. Successful
+ */
+
+int main(void)
+{
+	int i, j, x, y;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (x = i; x <= '9'; x++)
+			{
+				for (y = j; y <= '9'; y++)
+				{
+					if ((i == x) && (j == y))
+						continue;
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(x);
+					putchar(y);
+					if (j < '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
