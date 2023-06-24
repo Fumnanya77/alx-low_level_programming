068915642287a67d3441167368f3da29cfc1f11f
Frances7 <odohifunanyaj`gmail.com>
2023-06-24 14:03:26 +0100

    static libraries

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - to compare strings
+ *
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: an integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int cmpstr;
+
+	cmpstr = strcmp(s1, s2);
+
+	return (cmpstr);
+}
